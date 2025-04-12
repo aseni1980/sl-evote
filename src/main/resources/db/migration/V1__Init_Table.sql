42337f112c5e9501429932669494ddc9b16d06f0
Aseni Vimukthi <vimukthija@gmail.com>
2025-04-11 21:53:11 -0500

    Initial Commit

---
+++ src/main/resources/db/migration/V1__Init_Table.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users
+(
+    username VARCHAR(50)  NOT NULL PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    enabled  BOOLEAN      NOT NULL
+) ENGINE = InnoDb;
+CREATE TABLE authorities
+(
+    username  VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users (username),
+    UNIQUE INDEX authorities_idx_1 (username, authority)
+) ENGINE = InnoDb;
+
+CREATE TABLE customers
+(
+    id         BIGINT(50) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    firstname  VARCHAR(50)                         NOT NULL,
+    lastname   VARCHAR(255)                        NOT NULL,
+    email      VARCHAR(255)                        NOT NULL UNIQUE,
+    added_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+) ENGINE = InnoDb;
+
+INSERT INTO customers (id, firstname, lastname, email, added_date)
+VALUES (1, 'Vimukthi', 'Jayawardane', 'vimukthi@slevote.com', '2025-01-29 00:01:01'),
+       (2, 'Chanaka', 'Jayawardane', 'chanaka@slevote.com', '2025-01-29 00:01:01'),
+       (3, 'Nikini', 'Jayawardane', 'nikini@slevote.com', '2025-01-29 00:01:01'),
+       (4, 'Binara', 'Jayawardane', 'binara@slevote.com', '2025-01-29 00:01:01');
+
+ CREATE TABLE voters
+ (
+     username VARCHAR(50)  NOT NULL PRIMARY KEY,
+     password VARCHAR(255) NOT NULL,
+     enabled  BOOLEAN      NOT NULL
+ ) ENGINE = InnoDb;
